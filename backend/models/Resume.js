8df0c804543fa58fae0b2e38ef2a7b4c4e28fbc1
Lakmal <nipunamirandu@gmail.com>
2025-11-22 13:30:23 +0530

    model files

---
+++ backend/models/Resume.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const ResumeSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  title: String,
+  template: String,
+  resumeJson: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Resume", ResumeSchema);
